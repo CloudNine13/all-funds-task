d6aca4d11734b0c05de80d7c0d2e82efb46aad76
Igor Dzichkovskii <Igordzich@gmail.com>
2025-08-10 18:58:46 +0200

    fix(Model): Removed unneccesary virtual value and added image prop to database model

--- packages/backend/src/api/v1/constants.ts
+++ packages/backend/src/api/v1/constants.ts
@@ -1,10 +1,9 @@
 const NEWS_API_PATH = '/api/v1/news';
 const NEWS_MODEL_NAME = 'News';
-const IS_ARCHIVED = 'isArchived';
 const INTERNAL_SERVER_ERROR = 'Internal Server Error';
 
 const ROUTES = {
   ROOT: '/'
 };
 
-export { NEWS_API_PATH, NEWS_MODEL_NAME, IS_ARCHIVED, INTERNAL_SERVER_ERROR, ROUTES };
+export { NEWS_API_PATH, NEWS_MODEL_NAME, INTERNAL_SERVER_ERROR, ROUTES };
